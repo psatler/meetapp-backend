46632378fbb53ed347cfe3df973371f3d6165a64
psatler <pablosatler@gmail.com>
2019-08-28 22:36:01 -0300

    chore: install nodemon and sucrase, and change commonJS to import/export

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-const { Router } = require('express')
+import { Router } from 'express'
 
 const routes = new Router()
 
 routes.get('/', (req, res) => {
     return res.json({
-        message: 'Hello, World!'
+        message: 'Hello, World from sucrase-node!'
     })
 })
 
-module.exports = routes
+export default routes

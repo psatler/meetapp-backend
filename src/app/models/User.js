170422a606d1ec7ffd4c5fe14e6868296f6d8fd1
psatler <pablosatler@gmail.com>
2019-09-04 20:19:02 -0300

    feat: install bcryptjs and add hash to password before saving
    - using the hook `beforesave` to hash the user's password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,12 +8,21 @@ class User extends Model {
         // columns
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // this only exist on the code's side
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 

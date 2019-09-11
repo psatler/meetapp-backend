3355338b6a79d5abe95c17287e6865a2485b09e6
psatler <pablosatler@gmail.com>
2019-09-10 22:34:29 -0300

    feat: create the update logic for the UserController
    - update name, email and/or passwords

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -26,9 +26,33 @@ class UserController {
 
   // route protected by middleware
   async update(req, res) {
-    console.log(req.userId);
+    // get some values to perform validations
+    const { email, oldPassword } = req.body;
+    // get the user which is wanting to be edited in the database
+    const user = await User.findByPk(req.userId);
+
+    // if user is wanting to change email
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    // if user is wanting to change password
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      // if oldPassword is wrong
+      return res.status(401).json({ error: `Password does not match` });
+    }
+
+    // if everything passes, we update the user
+    const { id, name } = await user.update(req.body);
+
     return res.json({
-      ok: true,
+      id,
+      name,
+      email,
     });
   }
 }
